a9dae20ed810352d0d2954f843e266487ae81c44
Richard <richardmenning@googlemail.com>
2021-03-12 13:52:49 +0100

    started refactoring application

---
+++ udagram-api/feed-api/src/config/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
