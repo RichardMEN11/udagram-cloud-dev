76939167db06fca5fffbaef18861f33f85a60ed8
Richard <richardmenning@googlemail.com>
2021-03-14 19:38:43 +0100

    version with two seperate projects

--- udagram-api/src/controllers/v0/model.index.ts
+++
@@ -1,5 +0,0 @@
-import { FeedItem } from '../../../feed-api/src/controllers/v0/feed/models/FeedItem';
-import { User } from './users/models/User';
-
-export const V0_USER_MODELS = [User];
-export const V0_FEED_MODELS = [FeedItem];
--- udagram-api/users-api/src/config/config.ts
+++ udagram-api/users-api/src/config/config.ts
@@ -3,10 +3,7 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  dialect: 'postgres' as any,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
+  dialect: 'postgres',
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
